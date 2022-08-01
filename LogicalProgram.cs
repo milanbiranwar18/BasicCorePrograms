f5af8519a5b1ccbd42a5e7a4c34b5a1cfa7e7c79
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-01 11:12:27 +0530

    Validating that given input is Vowel or Consonant

--- LogicalProgram.cs
+++ LogicalProgram.cs
@@ -101,5 +101,20 @@ namespace BasicCorePrograms
                 }
             }
         }
+
+        public static void VowelConsonant()
+        {
+            Console.WriteLine("Enter any Character");
+            char ch = Convert.ToChar(Console.ReadLine());   
+
+            if ((ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch =='I' ||ch == 'O'|| ch == 'U'))
+            {
+                Console.WriteLine(ch + " is vowel");
+            }
+            else
+            {
+                Console.WriteLine(ch + " is Consonant");
+            }
+        }
     }
 }
